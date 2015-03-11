10302cdb1b78975adcb89bb9c9cb6d8444baee6c
Jennifer Duvall <jduvall330@gmail.com>
2015-03-11 12:29:38 -0400

    edits

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,6 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
-require 'mail'
+
 
 # Initialize the Rails application.
 Rails.application.initialize!
@@ -14,22 +14,3 @@ ActionMailer::Base.smtp_settings = {
 	:domain         => 'heroku.com',
 	:enable_starttls_auto => true
 }
-
-Mail.defaults do
-	delivery_method :smtp, {
-		:address => 'smtp.sendgrid.net',
-		:port => '587',
-		:domain => 'heroku.com',
-		:user_name => ENV['SENDGRID_USERNAME'],
-		:password => ENV['SENDGRID_PASSWORD'],
-		:authentication => :plain,
-		:enable_starttls_auto => true
-	}
-end
-
-Mail.deliver do
-	to 'example@example.com'
-	from 'sender@example.comt'
-	subject 'testing send mail'
-	body 'Sending email with Ruby through SendGrid!'
-end
