1ddab70d23ca98a5739b7ead48a8dc9c6c106575
Jennifer Duvall <jduvall330@gmail.com>
2015-03-11 10:52:10 -0400

    edited sendgrid

--- app/mailers/contact_form.rb
+++ app/mailers/contact_form.rb
@@ -11,6 +11,10 @@ class UserMailer < ActionMailer::Base
   	@name = params[:name]
   	@email = params[:email]
   	@message = params[:message]
-  	UserMailer.contact_form(@email, @name, @message).deliver
+  	# UserMailer.contact_form(@email, @name, @message).deliver
+    ActionMailer::Base.mail(:from => @email, 
+      :to => 'jduvall330@gmail.com', 
+      :subject => "A new contact form message from #{@name}", 
+      :body => @message).deliver
   end
 end
